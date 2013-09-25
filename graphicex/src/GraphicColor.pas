31b5bb514fd2e70b99af92bc1ee67c32bc95c539
Jacob Boerema <jgboerema@gmail.com>
2013-09-25 17:00:37 +0200

    Add: HalfToFloat and FloatToHalf functions for future 16 bit float TIF support.

--- graphicex/src/GraphicColor.pas
+++ graphicex/src/GraphicColor.pas
@@ -311,6 +311,18 @@ function MakeHLS(const H, L, S: Single): THLSFloat; overload;
 function MakeRGB(const R, G, B: Byte): TRGB; overload;
 function MakeRGB(const R, G, B: Single): TRGBFloat; overload;
 
+// HalfToFloat and FloatToHalf taken from ImagingLib (also in GlScene)
+type
+  { 16 bit floating-point value. It has 1 sign bit, 5 exponent bits,
+    and 10 mantissa bits.}
+  THalfFloat = type Word;
+  PHalfFloat = ^THalfFloat;
+
+{ Converts 16bit half floating point value to 32bit Single.}
+function HalfToFloat(Half: THalfFloat): Single;
+{ Converts 32bit Single to 16bit half floating point.}
+function FloatToHalf(Float: Single): THalfFloat;
+
 // general utility functions
 function ClampByte(Value: Integer): Byte;
 function MulDiv16(Number, Numerator, Denominator: Word): Word;
@@ -567,6 +579,175 @@ begin
       HLS2.S := HLS1.S + (HLS1.S - HLS2.S) * Ratio;
       Result := HLSToRGB(HLS2);
     end;
+end;
+
+
+{
+  Half/Float conversions inspired by half class from OpenEXR library.
+
+  Float (Pascal Single type) is an IEEE 754 single-precision
+  floating point number.
+
+  Bit layout of Single:
+
+    31 (msb)
+    |
+    | 30     23
+    | |      |
+    | |      | 22                    0 (lsb)
+    | |      | |                     |
+    X XXXXXXXX XXXXXXXXXXXXXXXXXXXXXXX
+    s e        m
+
+  Bit layout of half:
+
+    15 (msb)
+    |
+    | 14  10
+    | |   |
+    | |   | 9        0 (lsb)
+    | |   | |        |
+    X XXXXX XXXXXXXXXX
+    s e     m
+
+  S is the sign-bit, e is the exponent and m is the significand (mantissa).
+}
+
+function HalfToFloat(Half: THalfFloat): Single;
+var
+  Dst, Sign, Mantissa: LongWord;
+  Exp: LongInt;
+begin
+  // Extract sign, exponent, and mantissa from half number
+  Sign := Half shr 15;
+  Exp := (Half and $7C00) shr 10;
+  Mantissa := Half and 1023;
+
+  if (Exp > 0) and (Exp < 31) then
+  begin
+    // Common normalized number
+    Exp := Exp + (127 - 15);
+    Mantissa := Mantissa shl 13;
+    Dst := (Sign shl 31) or (LongWord(Exp) shl 23) or Mantissa;
+    // Result := Power(-1, Sign) * Power(2, Exp - 15) * (1 + Mantissa / 1024);
+  end
+  else if (Exp = 0) and (Mantissa = 0) then
+  begin
+    // Zero - preserve sign
+    Dst := Sign shl 31;
+  end
+  else if (Exp = 0) and (Mantissa <> 0) then
+  begin
+    // Denormalized number - renormalize it
+    while (Mantissa and $00000400) = 0 do
+    begin
+      Mantissa := Mantissa shl 1;
+      Dec(Exp);
+    end;
+    Inc(Exp);
+    Mantissa := Mantissa and not $00000400;
+    // Now assemble normalized number
+    Exp := Exp + (127 - 15);
+    Mantissa := Mantissa shl 13;
+    Dst := (Sign shl 31) or (LongWord(Exp) shl 23) or Mantissa;
+    // Result := Power(-1, Sign) * Power(2, -14) * (Mantissa / 1024);
+  end
+  else if (Exp = 31) and (Mantissa = 0) then
+  begin
+    // +/- infinity
+    Dst := (Sign shl 31) or $7F800000;
+  end
+  else //if (Exp = 31) and (Mantisa <> 0) then
+  begin
+    // Not a number - preserve sign and mantissa
+    Dst := (Sign shl 31) or $7F800000 or (Mantissa shl 13);
+  end;
+
+  // Reinterpret LongWord as Single
+  Result := PSingle(@Dst)^;
+end;
+
+function FloatToHalf(Float: Single): THalfFloat;
+var
+  Src: LongWord;
+  Sign, Exp, Mantissa: LongInt;
+begin
+  Src := PLongWord(@Float)^;
+  // Extract sign, exponent, and mantissa from Single number
+  Sign := Src shr 31;
+  Exp := LongInt((Src and $7F800000) shr 23) - 127 + 15;
+  Mantissa := Src and $007FFFFF;
+
+  if (Exp > 0) and (Exp < 30) then
+  begin
+    // Simple case - round the significand and combine it with the sign and exponent
+    Result := (Sign shl 15) or (Exp shl 10) or ((Mantissa + $00001000) shr 13);
+  end
+  else if Src = 0 then
+  begin
+    // Input float is zero - return zero
+    Result := 0;
+  end
+  else
+  begin
+    // Difficult case - lengthy conversion
+    if Exp <= 0 then
+    begin
+      if Exp < -10 then
+      begin
+        // Input float's value is less than HalfMin, return zero
+        Result := 0;
+      end
+      else
+      begin
+        // Float is a normalized Single whose magnitude is less than HalfNormMin.
+        // We convert it to denormalized half.
+        Mantissa := (Mantissa or $00800000) shr (1 - Exp);
+        // Round to nearest
+        if (Mantissa and $00001000) > 0 then
+          Mantissa := Mantissa + $00002000;
+        // Assemble Sign and Mantissa (Exp is zero to get denormalized number)
+        Result := (Sign shl 15) or (Mantissa shr 13);
+      end;
+    end
+    else if Exp = 255 - 127 + 15 then
+    begin
+      if Mantissa = 0 then
+      begin
+        // Input float is infinity, create infinity half with original sign
+        Result := (Sign shl 15) or $7C00;
+      end
+      else
+      begin
+        // Input float is NaN, create half NaN with original sign and mantissa
+        Result := (Sign shl 15) or $7C00 or (Mantissa shr 13);
+      end;
+    end
+    else
+    begin
+      // Exp is > 0 so input float is normalized Single
+
+      // Round to nearest
+      if (Mantissa and $00001000) > 0 then
+      begin
+        Mantissa := Mantissa + $00002000;
+        if (Mantissa and $00800000) > 0 then
+        begin
+          Mantissa := 0;
+          Exp := Exp + 1;
+        end;
+      end;
+
+      if Exp > 30 then
+      begin
+        // Exponent overflow - return infinity half
+        Result := (Sign shl 15) or $7C00;
+      end
+      else
+        // Assemble normalized half
+        Result := (Sign shl 15) or (Exp shl 10) or (Mantissa shr 13);
+    end;
+  end;
 end;
 
 //----------------- color utility functions ----------------------------------------------------------------------------
