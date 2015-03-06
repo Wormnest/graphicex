a20a235523038e4fb01acb4361e7eabc36eb3b3f
Jacob Boerema <jgboerema@gmail.com>
2015-03-06 23:58:02 +0100

    Start fpc compatibility branch adding fpc runtime packages. Since we need a working GraphicConfiguration.inc file I added it in the package folder.

---
+++ packages/externallibs.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ExternalLibs;
+
+interface
+
+uses
+  GXzLib, JPG, LibDelphi, LibJpegDelphi, LibStub, LibTiffDelphi, 
+  LibTiffDelphiHelper, TIFF, ZLibDelphi;
+
+implementation
+
+end.
