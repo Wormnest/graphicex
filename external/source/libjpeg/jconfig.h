6bea0d25b4d1d79ee8542fc5f4dc74fbee15523a
Jacob Boerema <jgboerema@gmail.com>
2015-03-13 21:09:28 +0100

    Change: External: Add makefiles and make jconfig mingw and bcc compatible.

--- external/source/libjpeg/jconfig.h
+++ external/source/libjpeg/jconfig.h
@@ -1,4 +1,4 @@
-/* jconfig.vc --- jconfig.h for Microsoft Visual C++ on Windows 95 or NT. */
+/* jconfig.vc --- Adapted from bcc version and generated mingw version. */
 /* see jconfig.doc for explanations */
 
 #define HAVE_PROTOTYPES
@@ -18,19 +18,29 @@
 #define NO_GETENV     /* disable use of environment variables (and scanf) */
 
 /* Define "boolean" as unsigned char, not int, per Windows custom */
+#if defined(WIN32) && !defined(__MINGW32__)
 #ifndef __RPCNDR_H__		/* don't conflict if rpcndr.h already read */
 typedef unsigned char boolean;
 #endif
 #define HAVE_BOOLEAN		/* prevent jmorecfg.h from redefining it */
+#endif
 
 #ifdef JPEG_INTERNALS
 
 #undef RIGHT_SHIFT_IS_UNSIGNED
 
+#if defined(__MINGW32__)
+#define INLINE __inline__
+/* These are for configuring the JPEG memory manager. */
+#undef DEFAULT_MAX_MEM
+#undef NO_MKTEMP
+#endif
+
 #endif /* JPEG_INTERNALS */
 
 #ifdef JPEG_CJPEG_DJPEG
 
+#if defined(__BORLANDC__)
 #undef BMP_SUPPORTED		/* BMP image file format */
 #undef GIF_SUPPORTED		/* GIF image file format */
 #undef PPM_SUPPORTED		/* PBMPLUS PPM/PGM image file format */
@@ -38,9 +48,24 @@ typedef unsigned char boolean;
 #undef TARGA_SUPPORTED		/* Targa image file format */
 
 #define TWO_FILE_COMMANDLINE	/* optional */
+
 #define USE_SETMODE		/* Microsoft has setmode() */
+#endif
+
+#if defined(__MINGW32__)
+#define BMP_SUPPORTED		/* BMP image file format */
+#define GIF_SUPPORTED		/* GIF image file format */
+#define PPM_SUPPORTED		/* PBMPLUS PPM/PGM image file format */
+#undef RLE_SUPPORTED		/* Utah RLE image file format */
+#define TARGA_SUPPORTED		/* Targa image file format */
+
+#undef TWO_FILE_COMMANDLINE
+#endif
+
+
 #undef NEED_SIGNAL_CATCHER
 #undef DONT_USE_B_MODE
+
 // GraphicEx special: want progress report.
 #define PROGRESS_REPORT		/* optional */
 
