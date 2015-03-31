f148de8a6c0663483b5b4a5f3edfe96d41f95c65
Jacob Boerema <jgboerema@gmail.com>
2015-03-31 11:37:06 +0200

    Tests: More ScaleConvert tests.

--- tests/graphicex/DU_GraphicColor_Tests.pas
+++ tests/graphicex/DU_GraphicColor_Tests.pas
@@ -34,19 +34,46 @@ type
     FCM: TCM;
     DoTestCounter: Integer;
     ScaleConvert16: function(Value: Word; BitsPerSampe: Byte): Byte of object;
+    ScaleConvert32: function(Value: LongWord; BitsPerSampe: Byte): Byte of object;
+    ScaleConvert64: function(Value: UInt64; BitsPerSampe: Byte): Byte of object;
     procedure SetUp; override;
     procedure TearDown; override;
     procedure DoTestMax16ToMax8(AValue: Word; ABitsPerSample: Byte;
       ExpectedResult: Byte);
+    procedure DoTestMax32ToMax8(AValue: LongWord; ABitsPerSample: Byte;
+      ExpectedResult: Byte);
+    procedure DoTestMax64ToMax8(AValue: UInt64; ABitsPerSample: Byte;
+      ExpectedResult: Byte);
   published
-    procedure CheckComponentScaleConvertUncommonTo8;
+    // Conversion to 4 bits per sample
     procedure CheckComponentScaleConvertTo4;
+    // Conversion to 8 bits per sample, 2 parameters
+    procedure CheckComponentScaleConvert1_15To8;
+    procedure CheckComponentScaleConvert6To8;
+    procedure CheckComponentScaleConvert10To8;
+    procedure CheckComponentScaleConvert12To8;
+    procedure CheckComponentScaleConvert14To8;
+    procedure CheckComponentScaleConvert32To8;
+    procedure CheckComponentScaleConvert64To8;
+    procedure CheckComponentScaleConvert17_31To8;
+    procedure CheckComponentScaleConvert33_63To8;
+    // Conversion to 8 bits per sample, 1 parameter
+    procedure CheckComponentScaleConvert16To8;
+    procedure CheckComponentScaleConvert32To8_1param;
+    procedure CheckComponentScaleConvert64To8_1param;
+    // Float conversion to 8 bits per sample
+    procedure CheckComponentScaleConvertFloat16To8;
+    procedure CheckComponentScaleConvertFloat24To8;
+    procedure CheckComponentScaleConvertFloat32To8;
+    procedure CheckComponentScaleConvertFloat64To8;
+    // Byte swap and scale conversion 16 to 18 bits
+    procedure CheckSwapScaleConvert;
   end;
 
 implementation
 
 uses SysUtils, Classes, Graphics,
-     GraphicStrings, graphicex;
+     GraphicEx;
 
 
 procedure TGetBitsTests.SetUp;
@@ -170,6 +197,8 @@ begin
   inherited;
   FCM := TCM.Create;
   ScaleConvert16 := nil;
+  ScaleConvert32 := nil;
+  ScaleConvert64 := nil;
 end;
 
 procedure TScaleConvertTests.TearDown;
@@ -184,6 +213,32 @@ var
   CvtResult: Byte;
 begin
   CvtResult := ScaleConvert16(AValue, ABitsPerSample);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
+procedure TScaleConvertTests.DoTestMax32ToMax8(AValue: LongWord; ABitsPerSample: Byte;
+  ExpectedResult: Byte);
+var
+  CvtResult: Byte;
+begin
+  CvtResult := ScaleConvert32(AValue, ABitsPerSample);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
+procedure TScaleConvertTests.DoTestMax64ToMax8(AValue: UInt64; ABitsPerSample: Byte;
+  ExpectedResult: Byte);
+var
+  CvtResult: Byte;
+begin
+  CvtResult := ScaleConvert64(AValue, ABitsPerSample);
   Inc(DoTestCounter);
   Check( CvtResult = ExpectedResult,
     Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
@@ -208,8 +263,25 @@ const
   Max14Bits = 16383;
   Max15Bits = 32765;
   Max16Bits = 65535;
+  Max32Bits = High(LongWord);
+  Max64Bits = High(UInt64);
 
-procedure TScaleConvertTests.CheckComponentScaleConvertUncommonTo8;
+  MaxBits17_32: array [17..32] of LongWord = (
+    1 shl 17 - 1, 1 shl 18 - 1, 1 shl 19 - 1, 1 shl 20 - 1, 1 shl 21 - 1,
+    1 shl 22 - 1, 1 shl 23 - 1, 1 shl 24 - 1, 1 shl 25 - 1, 1 shl 26 - 1,
+    1 shl 27 - 1, 1 shl 28 - 1, 1 shl 29 - 1, 1 shl 30 - 1, 1 shl 31 - 1,
+    High(LongWord));
+
+  MaxBits33_64: array [33..64] of UInt64 = (
+    UInt64(1) shl 33 - 1, UInt64(1) shl 34 - 1, UInt64(1) shl 35 - 1, UInt64(1) shl 36 - 1, UInt64(1) shl 37 - 1,
+    UInt64(1) shl 38 - 1, UInt64(1) shl 39 - 1, UInt64(1) shl 40 - 1, UInt64(1) shl 41 - 1, UInt64(1) shl 42 - 1,
+    UInt64(1) shl 43 - 1, UInt64(1) shl 44 - 1, UInt64(1) shl 45 - 1, UInt64(1) shl 46 - 1, UInt64(1) shl 47 - 1,
+    UInt64(1) shl 48 - 1, UInt64(1) shl 49 - 1, UInt64(1) shl 50 - 1, UInt64(1) shl 51 - 1, UInt64(1) shl 52 - 1,
+    UInt64(1) shl 53 - 1, UInt64(1) shl 54 - 1, UInt64(1) shl 55 - 1, UInt64(1) shl 56 - 1, UInt64(1) shl 57 - 1,
+    UInt64(1) shl 58 - 1, UInt64(1) shl 59 - 1, UInt64(1) shl 60 - 1, UInt64(1) shl 61 - 1, UInt64(1) shl 62 - 1,
+    UInt64(1) shl 63 - 1, High(UInt64));
+
+procedure TScaleConvertTests.CheckComponentScaleConvert1_15To8;
 begin
   ScaleConvert16 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvertUncommonTo8;
   DoTestCounter := 0;
@@ -261,6 +333,42 @@ begin
   // Convert 16 bits per sample to 8 bits
   DoTestMax16ToMax8(Max16Bits, 16, Max8Bits); // Max value 65535 --> 255
   DoTestMax16ToMax8(0, 16, 0);   // Min value 0 --? 0
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert6To8;
+begin
+  ScaleConvert16 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert6To8;
+  DoTestCounter := 0;
+  // Convert 6 bits per sample to 8 bits
+  DoTestMax16ToMax8(Max6Bits, 6, Max8Bits); // Max value 63 --> 255
+  DoTestMax16ToMax8(0, 6, 0);   // Min value 0 --? 0
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert10To8;
+begin
+  ScaleConvert16 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert10To8;
+  DoTestCounter := 0;
+  // Convert 10 bits per sample to 8 bits
+  DoTestMax16ToMax8(Max10Bits, 10, Max8Bits); // Max value
+  DoTestMax16ToMax8(0, 10, 0);   // Min value
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert12To8;
+begin
+  ScaleConvert16 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert12To8;
+  DoTestCounter := 0;
+  // Convert 12 bits per sample to 8 bits
+  DoTestMax16ToMax8(Max12Bits, 12, Max8Bits); // Max value
+  DoTestMax16ToMax8(0, 12, 0);   // Min value
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert14To8;
+begin
+  ScaleConvert16 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert14To8;
+  DoTestCounter := 0;
+  // Convert 14 bits per sample to 8 bits
+  DoTestMax16ToMax8(Max14Bits, 14, Max8Bits); // Max value
+  DoTestMax16ToMax8(0, 14, 0);   // Min value
 end;
 
 procedure TScaleConvertTests.CheckComponentScaleConvertTo4;
@@ -317,13 +425,329 @@ begin
   DoTestMax16ToMax8(0, 16, 0);                // Min value
 end;
 
+procedure TScaleConvertTests.CheckComponentScaleConvert17_31To8;
+var
+  BitsPerSample: Word;
+begin
+  DoTestCounter := 0;
+
+  // Both functions are essentially the same function and should be combined into 1!
+  ScaleConvert32 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert17_24To8;
+  for BitsPerSample := 17 to 24 do begin
+    DoTestMax32ToMax8(MaxBits17_32[BitsPerSample], BitsPerSample, Max8Bits); // Max value
+    DoTestMax32ToMax8(0, BitsPerSample, 0);                                  // Min value
+  end;
+  ScaleConvert32 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert25_31To8;
+  for BitsPerSample := 25 to 31 do begin
+    DoTestMax32ToMax8(MaxBits17_32[BitsPerSample], BitsPerSample, Max8Bits); // Max value
+    DoTestMax32ToMax8(0, BitsPerSample, 0);                                  // Min value
+  end;
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert32To8;
+begin
+  ScaleConvert32 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert32To8;
+  // Convert 32 bits per sample to 4 bits
+  DoTestMax32ToMax8(MaxBits17_32[32], 32, Max8Bits);   // Max value
+  DoTestMax32ToMax8(0, 32, 0);                  // Min value
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert64To8;
+begin
+  ScaleConvert64 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert64To8;
+  // Convert 64 bits per sample to 4 bits
+  DoTestMax64ToMax8(MaxBits33_64[64], 64, Max8Bits);   // Max value
+  DoTestMax64ToMax8(0, 64, 0);                  // Min value
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert33_63To8;
+var BitsPerSample: Word;
+begin
+  DoTestCounter := 0;
+  ScaleConvert64 := {$IFDEF FPC}@{$ENDIF}FCM.ComponentScaleConvert33_63To8;
+  for BitsPerSample := 33 to 63 do begin
+    DoTestMax64ToMax8(MaxBits33_64[BitsPerSample], BitsPerSample, Max8Bits); // Max value
+    DoTestMax64ToMax8(0, BitsPerSample, 0);                                  // Min value
+  end;
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert16To8;
+var
+  CvtResult,
+  ExpectedResult,
+  ABitsPerSample: Byte;
+  AValue: Word;
+begin
+  DoTestCounter := 0;
+  ABitsPerSample := 16;
+  AValue := Max16Bits;
+  ExpectedResult := Max8Bits;
+  CvtResult := FCM.ComponentScaleConvert16To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  AValue := 0;
+  ExpectedResult := 0;
+  CvtResult := FCM.ComponentScaleConvert16To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  // Extra test to make sure values in the middle of the range are also converted correctly
+  AValue := $8000;
+  ExpectedResult := 128;
+  CvtResult := FCM.ComponentScaleConvert16To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert32To8_1param;
+var
+  CvtResult,
+  ExpectedResult,
+  ABitsPerSample: Byte;
+  AValue: LongWord;
+begin
+  DoTestCounter := 0;
+  ABitsPerSample := 32;
+  AValue := Max32Bits;
+  ExpectedResult := Max8Bits;
+  CvtResult := FCM.ComponentScaleConvert32To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  AValue := 0;
+  ExpectedResult := 0;
+  CvtResult := FCM.ComponentScaleConvert32To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  // Extra test to make sure values in the middle of the range are also converted correctly
+  AValue := $80000000;
+  ExpectedResult := 128;
+  CvtResult := FCM.ComponentScaleConvert32To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvert64To8_1param;
+var
+  CvtResult,
+  ExpectedResult,
+  ABitsPerSample: Byte;
+  AValue: UInt64;
+begin
+  DoTestCounter := 0;
+  ABitsPerSample := 64;
+  AValue := Max64Bits;
+  ExpectedResult := Max8Bits;
+  CvtResult := FCM.ComponentScaleConvert64To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  AValue := 0;
+  ExpectedResult := 0;
+  CvtResult := FCM.ComponentScaleConvert64To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  // Extra test to make sure values in the middle of the range are also converted correctly
+  AValue := $8000000000000000;
+  ExpectedResult := 128;
+  CvtResult := FCM.ComponentScaleConvert64To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvertFloat16To8;
+var
+  CvtResult,
+  ExpectedResult,
+  ABitsPerSample: Byte;
+  AValue: Word;
+  AHalfFloat: THalfFloat absolute AValue;
+begin
+  DoTestCounter := 0;
+  ABitsPerSample := 16;
+  //AValue := Max16Bits;
+  AHalfFloat := FloatToHalf(1.0);
+  ExpectedResult := Max8Bits;
+  CvtResult := FCM.ComponentScaleConvertFloat16To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  //AValue := 0;
+  AHalfFloat := FloatToHalf(0.0);
+  ExpectedResult := 0;
+  CvtResult := FCM.ComponentScaleConvertFloat16To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvertFloat24To8;
+{var
+  CvtResult,
+  ExpectedResult,
+  ABitsPerSample: Byte;
+  AValue: LongWord;
+  AFloat24: TFloat24 absolute AValue;}
+begin
+  {$IFDEF IGNORE_EMPTY_TEST}
+  Self.Ignore('Test not Implemented!');
+  {$ELSE}
+  Check(False, 'Test not implemented!');
+  {$ENDIF}
+
+{ Todo: We need a FloatToFloat24 routine first
+  DoTestCounter := 0;
+  ABitsPerSample := 32;
+  //AValue := Max32Bits;
+  AFloat24 := FloatToFloat24(1.0);
+  ExpectedResult := Max8Bits;
+  CvtResult := FCM.ComponentScaleConvertFloat24To8(AValue, 24);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  //AValue := 0;
+  AFloat24 := FloatToFloat24(0.0);
+  ExpectedResult := 0;
+  CvtResult := FCM.ComponentScaleConvertFloat24To8(AValue, 24);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+}
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvertFloat32To8;
+var
+  CvtResult,
+  ExpectedResult,
+  ABitsPerSample: Byte;
+  AValue: LongWord;
+  ASingle: Single absolute AValue;
+begin
+  DoTestCounter := 0;
+  ABitsPerSample := 32;
+  //AValue := Max32Bits;
+  ASingle := 1.0;
+  ExpectedResult := Max8Bits;
+  CvtResult := FCM.ComponentScaleConvertFloat32To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  //AValue := 0;
+  ASingle := 0.0;
+  ExpectedResult := 0;
+  CvtResult := FCM.ComponentScaleConvertFloat32To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
+procedure TScaleConvertTests.CheckComponentScaleConvertFloat64To8;
+var
+  CvtResult,
+  ExpectedResult,
+  ABitsPerSample: Byte;
+  AValue: UInt64;
+  AFloat: Double absolute AValue;
+begin
+  DoTestCounter := 0;
+  ABitsPerSample := 64;
+  //AValue := Max64Bits;
+  AFloat := 1.0;
+  ExpectedResult := Max8Bits;
+  CvtResult := FCM.ComponentScaleConvertFloat64To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  //AValue := 0;
+  AFloat := 0.0;
+  ExpectedResult := 0;
+  CvtResult := FCM.ComponentScaleConvertFloat64To8(AValue);
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
+procedure TScaleConvertTests.CheckSwapScaleConvert;
+var
+  CvtResult,
+  ExpectedResult,
+  ABitsPerSample: Byte;
+  AValue: Word;
+begin
+  DoTestCounter := 0;
+  ABitsPerSample := 16;
+  // Swap is rtl function so we will trust it to be correct
+  AValue := Max16Bits;
+  ExpectedResult := Max8Bits;
+  CvtResult := FCM.ComponentSwapScaleConvert(Swap(AValue));
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  AValue := 0;
+  ExpectedResult := 0;
+  CvtResult := FCM.ComponentSwapScaleConvert(Swap(AValue));
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+  // Extra test to make sure values in the middle of the range are also converted correctly
+  AValue := $8000;
+  ExpectedResult := 128;
+  CvtResult := FCM.ComponentSwapScaleConvert(Swap(AValue));
+  Inc(DoTestCounter);
+  Check( CvtResult = ExpectedResult,
+    Format('Conversion test %d: Incorrect conversion result for value %d at %d bits per sample. ' +
+    'Result is %d but we expected %d.',
+    [DoTestCounter, AValue, ABitsPerSample, CvtResult, ExpectedResult]));
+end;
+
 initialization
   RegisterTests('Test GraphicEx.Unit GraphicColor',
     [
-      TGetBitsTests{$IFNDEF FPC}.Suite{$ENDIF}
-    ]);
-  RegisterTests('Test GraphicEx.Unit GraphicColor',
-    [
+      TGetBitsTests{$IFNDEF FPC}.Suite{$ENDIF},
       TScaleConvertTests{$IFNDEF FPC}.Suite{$ENDIF}
     ]);
 end.
