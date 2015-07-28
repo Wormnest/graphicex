2046c1551c9e1ba7bf8d778d64031907c963ab67
Jacob Boerema <jgboerema@gmail.com>
2015-07-28 16:23:16 +0200

    Change: Set heaptrc file logging as early as possible using HeaptrcLog unit.

--- tests/DU_graphicex_test.dpr
+++ tests/DU_graphicex_test.dpr
@@ -18,8 +18,8 @@ uses
   FastMM4,
   D6Support,                           // Ignore known memory leaks
   {$ELSE}
-  {$IFDEF HEAPTRC}
-  SysUtils,
+  {$IFDEF HEAPTRC_LOG}
+  HeaptrcLog,                          // Heaptrc logging should start as early as possible
   {$ENDIF}
   Forms, Interfaces,
   fpcunittestrunner,
@@ -35,12 +35,6 @@ begin
   {$IFNDEF FPC}
   GUITestRunner.RunRegisteredTests;
   {$ELSE}
-  {$IFDEF HEAPTRC}
-  // Set up -gh output for the Leakview package:
-  if FileExists('heaptrc.log') then
-    DeleteFile('heaptrc.log');
-  SetHeapTraceOutput('heaptrc.log');
-  {$ENDIF DEBUG}
   Application.Initialize;
   Application.CreateForm(TGuiTestRunner, TestRunner);
   Application.Run;
