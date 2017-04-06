9f7bd1de842bb4cf74e496f481cdcc69cb7c283e
Jacob Boerema <jgboerema@gmail.com>
2017-04-06 14:30:59 +0200

    Add, Tests: Two Jpeg saving tests. * TestSaveImage tests basic saving at different compression levels and if   no problems happen saving the same source several times. * TestSaveFromBmp tests saving from different source PixelFormats as jpeg. Note that these tests require you to check the output jpegs for problems.

--- tests/graphicex/du_jpegtests.pas
+++ tests/graphicex/du_jpegtests.pas
@@ -9,10 +9,11 @@ interface
 uses
   Windows, Classes, SysUtils, Graphics,
   {$IFNDEF FPC}
-  TestFramework
+  TestFramework,
   {$ELSE}
-  fpcunit, testregistry
+  fpcunit, testregistry,
   {$ENDIF}
+  GraphicColor
   ;
 
 type
@@ -22,10 +23,13 @@ type
     Graphic: TGraphic;
     procedure SetUp; override;
     procedure TearDown; override;
+    procedure TestBmp2Jpeg(ABmpName: string);
   published
     procedure TestCanLoad;
     procedure TestReadImageProperties;
     procedure TestLoadImage;
+    procedure TestSaveImage;
+    procedure TestSaveFromBmp;
   end;
 
 implementation
@@ -33,6 +37,9 @@ implementation
 uses GraphicEx, gexJpeg, TestSettings;
 
 var
+  BmpPath,
+  BmpImg,
+  JpegSavePath,
   Test1,
   Test2,
   Test3: string; // Location of test images.
@@ -109,13 +116,86 @@ begin
   AGraphic.Free;
 end;
 
+procedure TgexJpegImageTests.TestBmp2Jpeg(ABmpName: string);
+var
+  ABitmap: TBitmap;
+  AGraphic: TgexJpegImage;
+begin
+  ABitmap := TBitmap.Create;
+  AGraphic := TgexJpegImage.Create;
+  BmpImg := BmpPath + ABmpName;
+  ABitmap.LoadFromFile(BmpImg);
+  AGraphic.Assign(ABitmap);
+  AGraphic.SaveToFile(JpegSavePath + ABmpName + '.jpg');
+  AGraphic.Free;
+  ABitmap.Free
+end;
+
+procedure TgexJpegImageTests.TestSaveImage;
+var AGraphic: TgexJpegImage;
+  ABitmap: TBitmap;
+  w,h: Integer;
+  p: PRGB;
+begin
+  // Create correct graphic image format
+  AGraphic := TgexJpegImage.Create;
+  ABitmap := TBitmap.Create;
+  with ABitmap do begin
+    PixelFormat := pf24Bit;
+    Width := 64;
+    Height := 64;
+    for h := 0 to Height-1 do begin
+      p := ABitmap.ScanLine[h];
+      for w := 0 to Width-1 do begin
+        p^.R := 4*h-1;
+        p^.G := 2*w;
+        p^.B := w*h div 32;
+        Inc(p);
+      end;
+    end;
+  end;
+
+  AGraphic.Assign(ABitmap);
+  AGraphic.SaveToFile('TEMP_GEXJPEG_SAVETOFILE_TEST.jpg');
+  AGraphic.CompressionQuality := 25;
+  AGraphic.SaveToFile('TEMP_GEXJPEG_compression_25_TEST.jpg');
+  AGraphic.CompressionQuality := 50;
+  AGraphic.SaveToFile('TEMP_GEXJPEG_compression_50_TEST.jpg');
+  AGraphic.CompressionQuality := 75;
+  AGraphic.SaveToFile('TEMP_GEXJPEG_compression_75_TEST.jpg');
+
+  ABitmap.Free;
+  AGraphic.Free;
+end;
+
+procedure TgexJpegImageTests.TestSaveFromBmp;
+begin
+  // Use some of the images from the bmpsuite.
+  // Note this requires that you make a folder called temp below folder g.
+  TestBmp2Jpeg('pal1.bmp');
+  TestBmp2Jpeg('pal1bg.bmp');
+  TestBmp2Jpeg('pal4.bmp');
+  TestBmp2Jpeg('pal4gs.bmp');
+  TestBmp2Jpeg('pal8.bmp');
+  TestBmp2Jpeg('pal8gs.bmp');
+  TestBmp2Jpeg('rgb16.bmp');
+  TestBmp2Jpeg('rgb16bfdef.bmp');
+  TestBmp2Jpeg('rgb24.bmp');
+  TestBmp2Jpeg('rgb32.bmp');
+  TestBmp2Jpeg('rgb32bf.bmp');
+  TestBmp2Jpeg('rgb32bfdef.bmp');
+end;
+
 initialization
   // Set the location of our jpg test images.
   Test1 := ImagesBasePath + 'jpg\nuke-symbol.jpg';      // normal jpg
   Test2 := ImagesBasePath + 'jpg\unit-tests.xml';       // not a jpg
   Test3 := ImagesBasePath + 'jpg\wrong-extension.bmp';  // jpg with bmp extension
-  if not FileExists(Test1) or not FileExists(Test2) or not FileExists(Test3) then
-    MessageBox(0, 'At least one of the jpg test images does not exist!'#13#10+
+  BmpPath := ImagesBasePath + 'bmp\bmpsuite-2.5\g\';
+  BmpImg := BmpPath + 'rgb24.bmp';
+  JpegSavePath := BmpPath + 'temp\';
+  if not FileExists(Test1) or not FileExists(Test2) or not FileExists(Test3) or not FileExists(BmpImg) then
+    MessageBox(0, 'At least one of the test images does not exist!'#13#10+
       'Please change the name of the basepath or the test files to something valid for your system.',
       'Jpeg Tests', mb_iconhand + mb_ok)
   else
